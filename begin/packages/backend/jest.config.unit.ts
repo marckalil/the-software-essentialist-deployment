0726de9b335459397ea2a3b6a5b9ceac9b7b2949
Marc Kalil <marc@marckalil.com>
2025-08-01 18:38:41 +0200

    refactor: standardize Jest configuration files and add test setup script

--- begin/packages/backend/jest.config.unit.ts
+++ begin/packages/backend/jest.config.unit.ts
@@ -1,17 +1,18 @@
+import path from "path";
+import type { JestConfigWithTsJest } from "ts-jest";
+import { pathsToModuleNameMapper } from "ts-jest";
 
-import path from 'path';
-import type { JestConfigWithTsJest } from 'ts-jest';
-import { pathsToModuleNameMapper } from 'ts-jest';
-
-import { compilerOptions } from '../../tsconfig.json';
+import { compilerOptions } from "../../tsconfig.json";
 
 export default async (): Promise<JestConfigWithTsJest> => ({
-  displayName: 'Backend (Unit)',
-  testMatch: ['**/@(src|tests)/**/*.@(unit).*'],
+  displayName: "Backend (Unit)",
+  testMatch: ["**/@(src|tests)/**/*.@(unit).*"],
   transform: {
-    '^.+\\.(t|j)sx?$': ['ts-jest', {}],
+    "^.+\\.(t|j)sx?$": ["ts-jest", {}],
   },
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: path.resolve(__dirname, '../../'),
-  })
+    prefix: path.resolve(__dirname, "../../"),
+  }),
+  setupFilesAfterEnv: ["<rootDir>/tests/support/testSetup.ts"],
+  testEnvironment: "node",
 });
---
+++ begin/packages/backend/tests/support/testSetup.ts
@@ -0,0 +1,4 @@
+import "jest";
+
+// Extend Jest matchers if needed
+// This ensures that Jest globals like expect, describe, it, etc. are available
