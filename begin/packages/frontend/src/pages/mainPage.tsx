4f135fb858d0d32da8b87db75e078f6ca56c0f0a
Marc Kalil <marc@marckalil.com>
2025-08-06 01:10:09 +0200

    fix: type the posts state as Post[] and remove unnecessary ts-ignore comment

--- begin/packages/frontend/src/pages/mainPage.tsx
+++ begin/packages/frontend/src/pages/mainPage.tsx
@@ -4,14 +4,15 @@ import { PostsList } from "../components/postsList";
 import { PostsViewSwitcher } from "../components/postsViewSwitcher";
 import { api } from "../App";
 
+import { Post } from "@dddforum/shared/src/api/posts";
+
 export const MainPage = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
   const loadPosts = async () => {
     try {
       const response = await api.posts.getPosts("recent");
       console.log("Posts loaded:", response);
-      // @ts-ignore
-      setPosts(response.data ?? []);
+      setPosts(response.data);
     } catch (err) {
       console.log(err);
     }
