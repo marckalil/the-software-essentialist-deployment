6b645a46a12e1f87be62f537cc9714cdb5579683
Marc Kalil <marc@marckalil.com>
2025-08-06 00:10:30 +0200

    fix: implement post formatting and update findPosts method to return formatted posts

--- begin/packages/backend/src/modules/posts/adapters/productionPostsRepository.ts
+++ begin/packages/backend/src/modules/posts/adapters/productionPostsRepository.ts
@@ -1,10 +1,39 @@
 import { PrismaClient } from "@prisma/client";
+
 import { Post } from "@dddforum/shared/src/api/posts";
+
 import { PostsRepository } from "../ports/postsRepository";
+import { ServerErrorException } from "../../../shared/exceptions";
 
 export class ProductionPostsRepository implements PostsRepository {
   constructor(private prisma: PrismaClient) {}
-  async findPosts(_sort: string): Promise<Post[]> {
-    return [];
+
+  private formatPost(post: any): Post {
+    return {
+      id: post.id,
+      memberId: post.memberId,
+      postType: post.postType,
+      title: post.title,
+      content: post.content,
+      dateCreated: post.dateCreated.toISOString(),
+    };
+  }
+
+  /**
+   * Finds posts sorted by the specified criteria.
+   * @param sort - The sorting criteria (not used in this implementation).
+   * @returns A promise that resolves to an array of formatted posts.
+   */
+  async findPosts(_: string): Promise<Post[]> {
+    try {
+      const posts = await this.prisma.post.findMany({
+        orderBy: { dateCreated: "desc" },
+      });
+      const formattedPosts = posts.map(this.formatPost);
+
+      return formattedPosts;
+    } catch (error) {
+      throw new ServerErrorException();
+    }
   }
 }
