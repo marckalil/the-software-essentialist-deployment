824b631b5234fee212e56c695035bf4862f9db77
Marc Kalil <marc@marckalil.com>
2025-08-06 22:04:57 +0200

    fix: update import statements and improve test setup in registration.e2e.ts

--- begin/packages/frontend/tests/features/registration.e2e.ts
+++ begin/packages/frontend/tests/features/registration.e2e.ts
@@ -1,30 +1,35 @@
-
-import { defineFeature, loadFeature } from 'jest-cucumber';
-import { sharedTestRoot } from '@dddforum/shared/src/paths';
-import { CreateUserBuilder } from "@dddforum/shared/tests/support/builders/createUserBuilder";
+import { defineFeature, loadFeature } from "jest-cucumber";
+import { sharedTestRoot } from "@dddforum/shared/src/paths";
+import { UserBuilder } from "@dddforum/shared/tests/support/builders/users";
 import { CreateUserParams } from "@dddforum/shared/src/api/users";
-import { Pages } from '../support/pages/pages';
+import { Pages } from "../support/pages/pages";
 
-import * as path from 'path';
-import { PuppeteerPageDriver } from '../support/driver';
-import { App, createAppObject } from '../support/pages';
-import { DatabaseFixture } from '@dddforum/shared/tests/support/fixtures/databaseFixture';
+import * as path from "path";
+import { PuppeteerPageDriver } from "../support/driver";
+import { App, createAppObject } from "../support/pages";
+import { DatabaseFixture } from "@dddforum/shared/tests/support/fixtures/databaseFixture";
+import { Config } from "@dddforum/backend/src/shared/config";
+import { CompositionRoot } from "@dddforum/backend/src/shared/compositionRoot";
 
-const feature = loadFeature(path.join(sharedTestRoot, 'features/registration.feature'), { tagFilter: '@frontend' });
-
+const feature = loadFeature(
+  path.join(sharedTestRoot, "features/registration.feature"),
+  { tagFilter: "@frontend" },
+);
 
 defineFeature(feature, (test) => {
-  let app: App
+  let app: App;
   let pages: Pages;
   let puppeteerPageDriver: PuppeteerPageDriver;
   let user: CreateUserParams;
   let users: CreateUserParams[];
-  let databaseFixture: DatabaseFixture
-
+  let databaseFixture: DatabaseFixture;
+  let composition: CompositionRoot;
+  const config: Config = new Config("test:e2e");
 
   beforeAll(async () => {
-    databaseFixture = new DatabaseFixture();
-    puppeteerPageDriver = await PuppeteerPageDriver.create({ 
+    composition = CompositionRoot.createCompositionRoot(config);
+    databaseFixture = new DatabaseFixture(composition);
+    puppeteerPageDriver = await PuppeteerPageDriver.create({
       headless: false,
       slowMo: 50,
     });
@@ -43,86 +48,117 @@ defineFeature(feature, (test) => {
   // Need to put timeout here.
   jest.setTimeout(60000);
 
-  test('Successful registration with marketing emails accepted', ({ given, when, then, and }) => {
-
-    given('I am a new user', async () => {
-      user = new CreateUserBuilder()
+  test("Successful registration with marketing emails accepted", ({
+    given,
+    when,
+    then,
+    and,
+  }) => {
+    given("I am a new user", async () => {
+      user = new UserBuilder()
+        .makeCreateUserCommandBuilder()
         .withAllRandomDetails()
         .build();
-      
+
       await pages.registration.open();
       await pages.registration.acceptMarketingEmails();
     });
 
-    when('I register with valid account details accepting marketing emails', async () => {
-      await pages.registration.enterAccountDetails(user);
-      await pages.registration.submitRegistrationForm();
-    });
+    when(
+      "I register with valid account details accepting marketing emails",
+      async () => {
+        await pages.registration.enterAccountDetails(user);
+        await pages.registration.submitRegistrationForm();
+      },
+    );
 
-    then('I should be granted access to my account', async () => {
+    then("I should be granted access to my account", async () => {
       expect(await app.header.getUsernameFromHeader()).toContain(user.username);
     });
 
-    and('I should expect to receive marketing emails', () => {
-      // @See backend 
+    and("I should expect to receive marketing emails", () => {
+      // @See backend
     });
   });
 
-
-  test('Invalid or missing registration details', ({ given, when, then, and }) => {
-    given('I am a new user', async () => {
-      user = new CreateUserBuilder()
+  test("Invalid or missing registration details", ({
+    given,
+    when,
+    then,
+    and,
+  }) => {
+    given("I am a new user", async () => {
+      user = new UserBuilder()
+        .makeCreateUserCommandBuilder()
         .withAllRandomDetails()
-        .withEmail('skj')
+        .withEmail("skj")
         .build();
-    
+
       await pages.registration.open();
       await pages.registration.acceptMarketingEmails();
     });
 
-    when('I register with invalid account details', async () => {
+    when("I register with invalid account details", async () => {
       await pages.registration.enterAccountDetails(user);
       await pages.registration.submitRegistrationForm();
     });
 
-    then('I should see an error notifying me that my input is invalid', async () => {
-      expect(await app.notifications.getErrorNotificationText()).toBeDefined();
-      expect(await app.notifications.getErrorNotificationText()).toContain('invalid');
-    });
+    then(
+      "I should see an error notifying me that my input is invalid",
+      async () => {
+        expect(
+          await app.notifications.getErrorNotificationText(),
+        ).toBeDefined();
+        expect(await app.notifications.getErrorNotificationText()).toContain(
+          "invalid",
+        );
+      },
+    );
 
-    and('I should not have been sent access to account details', () => {
-      // @See backend 
+    and("I should not have been sent access to account details", () => {
+      // @See backend
     });
   });
 
-  test('Account already created with email', ({ given, when, then, and }) => {
-    given('a set of users already created accounts', async (table: CreateUserParams[]) => {
-      users = table.map((user) => {
-        return new CreateUserBuilder()
-          .withAllRandomDetails()
-          .withEmail(user.email)
-          .withFirstName(user.firstName)
-          .withLastName(user.lastName)
-          .build();
-      });
+  test("Account already created with email", ({ given, when, then, and }) => {
+    given(
+      "a set of users already created accounts",
+      async (table: CreateUserParams[]) => {
+        users = table.map((user) => {
+          return new UserBuilder()
+            .makeCreateUserCommandBuilder()
+            .withAllRandomDetails()
+            .withEmail(user.email)
+            .withFirstName(user.firstName)
+            .withLastName(user.lastName)
+            .build();
+        });
 
-      await databaseFixture.setupWithExistingUsers(users);
-      await pages.registration.open();
-      await pages.registration.acceptMarketingEmails();
-    });
-  
-    when('new users attempt to register with those emails', async () => {
+        await databaseFixture.setupWithExistingUsers(users);
+        await pages.registration.open();
+        await pages.registration.acceptMarketingEmails();
+      },
+    );
+
+    when("new users attempt to register with those emails", async () => {
       await pages.registration.enterAccountDetails(users[0]);
       await pages.registration.submitRegistrationForm();
     });
-  
-    then('they should see an error notifying them that the account already exists', async () => {
-      expect(await app.notifications.getErrorNotificationText()).toBeDefined();
-      expect(await app.notifications.getErrorNotificationText()).toContain('in use');
-    });
 
-    and('they should not have been sent access to account details', () => {
-      // @See backend 
-    })
+    then(
+      "they should see an error notifying them that the account already exists",
+      async () => {
+        expect(
+          await app.notifications.getErrorNotificationText(),
+        ).toBeDefined();
+        expect(await app.notifications.getErrorNotificationText()).toContain(
+          "in use",
+        );
+      },
+    );
+
+    and("they should not have been sent access to account details", () => {
+      // @See backend
+    });
   });
 });
